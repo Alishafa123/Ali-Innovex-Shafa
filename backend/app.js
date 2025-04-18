b558d1132b3f1a6c7934be5122cef3fde997a2ef
Muhammad Ali Shafa <116679960+Alishafa123@users.noreply.github.com>
2025-04-18 16:31:18 +0500

    Cors dependency added

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const urlRoutes = require('./routes/urlRoutes');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use('/shortens', urlRoutes);
 
 module.exports = app;
